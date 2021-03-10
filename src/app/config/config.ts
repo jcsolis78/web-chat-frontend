198fac56160f6097c44de411024d3dc1b04af1f0
Jhon Solis <jcsolis@dcatech.com>
2021-03-10 14:29:53 -0500

    Cambios#

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_BACKEND = 'https://springboot-jcso-web-chat.herokuapp.com'; 
+//export const URL_BACKEND = 'http://springboot-jcso-web-chat.herokuapp.com'; 
+export const URL_BACKEND = 'http://localhost:8080'; 
